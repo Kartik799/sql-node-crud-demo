da9988fc515f7f34ac43a1d9c28ee1007185dd61
Kartik Singh <121419609+Kartik799@users.noreply.github.com>
2025-08-17 16:39:56 +0530

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
--- CREATE TABLE user (
---     id varchar(50) PRIMARY KEY,
---     username varchar(50) UNIQUE,
---     email varchar(50) UNIQUE NOT NULL,
---     password varchar(50) NOT NULL
--- );
-
-SELECT* FROM user WHERE email ="Dorothy46@hotmail.com"
